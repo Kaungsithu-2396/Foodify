d85c3ec261ad752fe2037522cf4fc65c962f6a71
Kaungsithu-123 <Kaungsithu2396@gmail.com>
2023-05-19 22:39:52 +0630

    Preparing API

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FetchData from "./Components/fetchData";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <FetchData />
+        </>
+    );
+}
+
+export default App;
